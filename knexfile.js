822cc7d817e5e2a6fe41146e492ea3bb99d42cab
badCompany55 <zachirvin45@gmail.com>
2019-04-25 18:07:24 -0500

    pg

--- knexfile.js
+++ knexfile.js
@@ -17,12 +17,13 @@ module.exports = {
 
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL
-  },
-  migrations: {
-    directory: __dirname + "/data/migrations/"
-  },
-  seeds: {
-    directory: "./data/seeds/"
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./data/migrations/"
+    },
+    seeds: {
+      directory: "./data/seeds/"
+    },
+    useNullAsDefault: true
   }
 };
