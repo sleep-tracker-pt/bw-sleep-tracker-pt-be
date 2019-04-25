bff48d6c223ac96da70784b9975afb8b08e09bbc
badCompany55 <zachirvin45@gmail.com>
2019-04-25 17:12:50 -0500

    prod

--- knexfile.js
+++ knexfile.js
@@ -32,7 +32,7 @@ module.exports = {
   },
 
   production: {
-    client: "postgresql",
+    client: "pg",
     connection: {
       database: "d4k99jd8rkd18t",
       user: "ykimldzoyengoq",
@@ -44,7 +44,10 @@ module.exports = {
       max: 10
     },
     migrations: {
-      tableName: "knex_migrations"
+      directory: "./data/migrations/"
+    },
+    seeds: {
+      directory: "./data/seeds/"
     }
   }
 };
