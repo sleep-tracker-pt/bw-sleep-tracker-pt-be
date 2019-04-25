3693800959e4b86a74cd834e4d37eda84cf51f16
badCompany55 <zachirvin45@gmail.com>
2019-04-25 17:51:35 -0500

    pg

--- knexfile.js
+++ knexfile.js
@@ -15,28 +15,12 @@ module.exports = {
     useNullAsDefault: true
   },
 
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  },
-
   production: {
     client: "pg",
     connection: process.env.DATABASE_URL
   },
   migrations: {
-    directory: "./data/migrations/"
+    directory: __dirname + "/data/migrations/"
   },
   seeds: {
     directory: "./data/seeds/"
